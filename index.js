ff5a1ff2aabb231a505457653954388c882009b1
Siim Ots <siimots@gmail.com>
2024-03-01 17:31:03 +0200

    Update package.json

--- index.js
+++ index.js
@@ -84,121 +84,114 @@ const fetchPort = async (port) => {
 
   const url = `https://www.sadamaregister.ee/ports/${id}/json`;
 
-  try {
-    const response = await fetch(url, options);
-    const json = await response.json();
+  const response = await fetch(url, options);
+  const json = await response.json();
 
-    const { portMainData } = json;
+  const { portMainData } = json;
 
-    if (!portMainData) {
-      console.log("Missing json", id, name);
-      return;
-    }
-
-    //console.log(json);
-
-    let {
-      sadamaPidajaEesnimi,
-      sadamaPidajaArinimiPerenimi,
-      sadamaPidajaTelefon,
-      sadamaPidajaEpost,
-      sadamaPidajaKoduleht,
-
-      sadamaKaptenEesnimi,
-      sadamaKaptenPerenimi,
-      sadamaKaptenTelefon,
-      sadamaKaptenEpost,
-
-      veesoidukiMaxPikkus,
-      veesoidukiMaxLaius,
-      veesoidukiMaxSyvis,
-
-      muutmineKp,
-    } = portMainData;
-
-    let omanik = "-";
-    if (sadamaPidajaEesnimi && sadamaPidajaArinimiPerenimi) {
-      omanik = sadamaPidajaEesnimi + " " + sadamaPidajaArinimiPerenimi;
-    } else if (sadamaPidajaArinimiPerenimi) {
-      omanik = sadamaPidajaArinimiPerenimi;
-    }
-
-    let omanik_telefon = "-";
-    if (sadamaPidajaTelefon && sadamaPidajaTelefon.length !== 0) {
-      omanik_telefon = sadamaPidajaTelefon.join(";");
-    }
-
-    let omanik_epost = "-";
-    if (sadamaPidajaEpost) {
-      omanik_epost = sadamaPidajaEpost;
-    }
-
-    let koduleht = "-";
-    if (sadamaPidajaKoduleht) {
-      koduleht = sadamaPidajaKoduleht;
-    }
-
-    let sadamakapteni_nimi = "-";
-    if (sadamaKaptenEesnimi && sadamaKaptenPerenimi) {
-      sadamakapteni_nimi = sadamaKaptenEesnimi + " " + sadamaKaptenPerenimi;
-    }
-
-    let sadamakapteni_telefon = "-";
-    if (sadamaKaptenTelefon && sadamaKaptenTelefon.length !== 0) {
-      sadamakapteni_telefon = sadamaKaptenTelefon.join(";");
-    }
-
-    let sadamakapteni_epost = "-";
-    if (sadamaKaptenEpost) {
-      sadamakapteni_epost = sadamaKaptenEpost;
-    }
-
-    let max_pikkus = "-";
-    if (veesoidukiMaxPikkus) {
-      max_pikkus = veesoidukiMaxPikkus;
-    }
-
-    let max_laius = "-";
-    if (veesoidukiMaxLaius) {
-      max_laius = veesoidukiMaxLaius;
-    }
-
-    let max_sygavus = "-";
-    if (veesoidukiMaxSyvis) {
-      max_sygavus = veesoidukiMaxSyvis;
-    }
-
-    let coords = portMainData.sadamaAsukoht.split(";");
-    coords = coords.map((str) => str.trim());
-    const [lat, lon] = coords.map(dmsConversion.parseDms);
-
-    let feature = {
-      type: "Feature",
-      properties: {
-        sadamaregister: id,
-        sadama_nimi: name,
-        omanik: omanik,
-        omanik_telefon: omanik_telefon,
-        omanik_epost: omanik_epost,
-        koduleht: koduleht,
-        sadamakapteni_nimi: sadamakapteni_nimi,
-        sadamakapteni_telefon: sadamakapteni_telefon,
-        sadamakapteni_epost: sadamakapteni_epost,
-        max_pikkus: max_pikkus,
-        max_laius: max_laius,
-        max_sygavus: max_sygavus,
-        modified_date: muutmineKp,
-      },
-      geometry: {
-        type: "Point",
-        coordinates: [+lon.toFixed(5), +lat.toFixed(5)],
-      },
-    };
-
-    return feature;
-  } catch (error) {
-    console.log(id, name, error);
+  if (!portMainData) {
+    throw `Missing json ${id} ${name}`;
   }
+
+  let {
+    sadamaPidajaEesnimi,
+    sadamaPidajaArinimiPerenimi,
+    sadamaPidajaTelefon,
+    sadamaPidajaEpost,
+    sadamaPidajaKoduleht,
+
+    sadamaKaptenEesnimi,
+    sadamaKaptenPerenimi,
+    sadamaKaptenTelefon,
+    sadamaKaptenEpost,
+
+    veesoidukiMaxPikkus,
+    veesoidukiMaxLaius,
+    veesoidukiMaxSyvis,
+
+    muutmineKp,
+  } = portMainData;
+
+  let omanik = "-";
+  if (sadamaPidajaEesnimi && sadamaPidajaArinimiPerenimi) {
+    omanik = sadamaPidajaEesnimi + " " + sadamaPidajaArinimiPerenimi;
+  } else if (sadamaPidajaArinimiPerenimi) {
+    omanik = sadamaPidajaArinimiPerenimi;
+  }
+
+  let omanik_telefon = "-";
+  if (sadamaPidajaTelefon && sadamaPidajaTelefon.length !== 0) {
+    omanik_telefon = sadamaPidajaTelefon.join(";");
+  }
+
+  let omanik_epost = "-";
+  if (sadamaPidajaEpost) {
+    omanik_epost = sadamaPidajaEpost;
+  }
+
+  let koduleht = "-";
+  if (sadamaPidajaKoduleht) {
+    koduleht = sadamaPidajaKoduleht;
+  }
+
+  let sadamakapteni_nimi = "-";
+  if (sadamaKaptenEesnimi && sadamaKaptenPerenimi) {
+    sadamakapteni_nimi = sadamaKaptenEesnimi + " " + sadamaKaptenPerenimi;
+  }
+
+  let sadamakapteni_telefon = "-";
+  if (sadamaKaptenTelefon && sadamaKaptenTelefon.length !== 0) {
+    sadamakapteni_telefon = sadamaKaptenTelefon.join(";");
+  }
+
+  let sadamakapteni_epost = "-";
+  if (sadamaKaptenEpost) {
+    sadamakapteni_epost = sadamaKaptenEpost;
+  }
+
+  let max_pikkus = "-";
+  if (veesoidukiMaxPikkus) {
+    max_pikkus = veesoidukiMaxPikkus;
+  }
+
+  let max_laius = "-";
+  if (veesoidukiMaxLaius) {
+    max_laius = veesoidukiMaxLaius;
+  }
+
+  let max_sygavus = "-";
+  if (veesoidukiMaxSyvis) {
+    max_sygavus = veesoidukiMaxSyvis;
+  }
+
+  let coords = portMainData.sadamaAsukoht.split(";");
+  coords = coords.map((str) => str.trim());
+  const [lat, lon] = coords.map(dmsConversion.parseDms);
+
+  let feature = {
+    type: "Feature",
+    properties: {
+      sadamaregister: id,
+      sadama_nimi: name,
+      omanik: omanik,
+      omanik_telefon: omanik_telefon,
+      omanik_epost: omanik_epost,
+      koduleht: koduleht,
+      sadamakapteni_nimi: sadamakapteni_nimi,
+      sadamakapteni_telefon: sadamakapteni_telefon,
+      sadamakapteni_epost: sadamakapteni_epost,
+      max_pikkus: max_pikkus,
+      max_laius: max_laius,
+      max_sygavus: max_sygavus,
+      modified_date: muutmineKp,
+    },
+    geometry: {
+      type: "Point",
+      coordinates: [+lon.toFixed(5), +lat.toFixed(5)],
+    },
+  };
+
+  return feature;
 };
 
 fetchPorts();
